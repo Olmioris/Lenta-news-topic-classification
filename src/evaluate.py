42f9ae4ea3b7f5129ce67828c66bb47a26146f0c
Olmioris <olmioris@gmail.com>
2026-07-04 20:22:37 +0300

    Update evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,1 +1,10 @@
+import numpy as np
+from sklearn.metrics import accuracy_score, f1_score, classification_report
 
+def evaluate_model(model, X_test, y_test):
+    y_pred = model.predict(X_test)
+
+    print("Test accuracy:", accuracy_score(y_test, y_pred))
+    print("Test macro F1:", f1_score(y_test, y_pred, average="macro"))
+    print()
+    print(classification_report(y_test, y_pred))

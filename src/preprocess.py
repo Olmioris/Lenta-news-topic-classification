fd80ec06a9ec7e3974e700b26977a0f690183495
Olmioris <olmioris@gmail.com>
2026-07-04 20:20:04 +0300

    Update preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,1 +1,18 @@
+import re
+from pymorphy3 import MorphAnalyzer
 
+morph = MorphAnalyzer()
+
+def preprocess_text(text: str) -> str:
+    # нижний регистр
+    text = text.lower()
+
+    # оставляем только буквы и пробелы
+    text = re.sub(r"[^а-яёa-z\s]", " ", text)
+    text = re.sub(r"\s+", " ", text).strip()
+
+    # лемматизация
+    tokens = text.split()
+    lemmas = [morph.parse(token)[0].normal_form for token in tokens]
+
+    return " ".join(lemmas)
